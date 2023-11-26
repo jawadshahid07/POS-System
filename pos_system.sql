45caa78aa3302f7fd1eb1c235b0ecd259bc96b57
jawadshahid07 <jawad.shahid70@outlook.com>
2023-11-26 18:38:01 +0500

    finalize sql file

--- pos_system.sql
+++ pos_system.sql
@@ -22,7 +22,6 @@ CREATE TABLE categories (
     name VARCHAR(255),
     description VARCHAR(255)
 );
-
 -- for maintaining products in database
 CREATE TABLE products (
     code INT AUTO_INCREMENT PRIMARY KEY,
@@ -31,5 +30,22 @@ CREATE TABLE products (
     stockQuantity INT,
     price DECIMAL(10, 2),
     categoryCode INT,
+    alertQuantity INT, 
+    dateTracked VARCHAR(255),
     FOREIGN KEY (categoryCode) REFERENCES categories(code) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE orders (
+    orderID INT AUTO_INCREMENT PRIMARY KEY,
+    totalAmount DOUBLE,
+    orderDate VARCHAR(255)
+);
+
+CREATE TABLE orderedItems (
+    itemID INT PRIMARY KEY AUTO_INCREMENT,
+    orderID INT,
+    productCode INT ,
+    quantityOrdered INT,
+    FOREIGN KEY (orderID) REFERENCES orders(orderID) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (productcode) REFERENCES products(code) ON UPDATE CASCADE ON DELETE CASCADE
 );
